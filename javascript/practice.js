9aee538d4bf21e097fe74345426028a42c5eec4c
sanskruti chavan <sanskrutichavan45@gmail.com>
2025-08-01 19:14:48 +0530

    Add files via upload

---
+++ javascript/practice.js
@@ -0,0 +1,33 @@
+//2a2b2c
+
+
+
+
+
+
+let num = "2a2b2c";
+console.log(num);
+
+let num1 ="sanskruti";
+let place ="   pune     ";
+console.log("length of num1 : "+num1.length);//check length
+console.log("num1 uppercase : "+num1.toUpperCase());//convert to upper
+console.log("num1 charAt: "+num1.charAt(0));//index nums charecter give
+console.log(" num1 indexof: "+num1.indexOf("n"));//index num gives
+console.log(" num1 includes: "+num1.includes("p"));//is it availble or not check
+console.log(" num1 startwith: "+num1.startsWith("p"));//start value check
+console.log(" num1 endswith: "+num1.endsWith("i"));//end check
+
+console.log(" num1 slice: "+num1.slice(4,8));//cut pic
+console.log(" num1 substring: "+num1.substring(4,8));//give cut pic
+console.log(" num1 replace: "+num1.replace("i","ss"));//1st char replce 2nd
+console.log(" num1 trim: "+place.trim());//remove start and ending space
+
+
+
+
+
+
+
+
+
